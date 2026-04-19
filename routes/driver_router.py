ad502367d38d37d2ae711f08613c90b5a463cc82
Braydon <braydonru1234@gmail.com>
2026-04-18 23:28:24 -0500

    websocket

--- routes/driver_router.py
+++ routes/driver_router.py
@@ -1,21 +1,36 @@
 from http.client import HTTPException
-from typing import List
-from fastapi import APIRouter, exceptions
+from fastapi import APIRouter, exceptions, WebSocket, WebSocketDisconnect
 from config.security import hash_password
 from .Deps.db_session import SessionDep
 from models.driver import Driver, DriverCreateIn, DriverCreateOut
 from sqlmodel import select
-
+import asyncio
 
 driver_router = APIRouter(tags=['Drivers'],prefix="/drivers")
 
-@driver_router.get("/get_drivers", response_model=List[DriverCreateOut])
-def get_drivers(db:SessionDep)->List[DriverCreateOut]:
-    res = select(Driver).filter(Driver.Driving == True)
-    drivers = db.exec(res).all()
-    return [DriverCreateOut(name=d.name, cellphone=d.cellphone, longitude=d.longitude, latitude=d.latitude) for d in drivers]
 
+@driver_router.websocket("/get_drivers")
+async def get_drivers(websocket: WebSocket, db: SessionDep):
+    await websocket.accept()
+    try:
+        while True:
+            statement = select(Driver).where(Driver.Driving == True)
+            drivers = db.exec(statement).all()
 
+            drivers_data = [
+                {
+                    "id": d.id,
+                    "name": d.name,
+                    "latitude": d.latitude,
+                    "longitude": d.longitude
+                }
+                for d in drivers
+            ]
+            await websocket.send_json(drivers_data)
+            await asyncio.sleep(2)
+
+    except WebSocketDisconnect:
+        print("Cliente desconectado")
 
 @driver_router.post("/create_driver")
 def create_driver(driver:DriverCreateIn,db:SessionDep):
@@ -57,13 +72,28 @@ def set_driving(db:SessionDep, id:int):
     return D
 
 
-@driver_router.put("/modify_position")
-def modify_position(db:SessionDep, lat:float,lon:float,id:int):
-    D = db.get(Driver, id)
-    if not D:
-        raise exceptions.HTTPException(status_code=404, detail="Driver not found")
-    D.latitude = lat
-    D.longitude = lon
-    db.commit()
-    db.refresh(D)
-    return D
+active_connections = {}
+
+
+@driver_router.websocket("/driver/position")
+async def driver_position(websocket: WebSocket, driver_id: int, db: SessionDep):
+    await websocket.accept()
+    active_connections[driver_id] = websocket
+
+    try:
+        while True:
+            data = await websocket.receive_json()
+            lat = data.get("latitude")
+            lon = data.get("longitude")
+
+            driver = db.get(Driver, driver_id)
+            if driver:
+                driver.latitude = lat
+                driver.longitude = lon
+                db.commit()
+                db.refresh(driver)
+
+                await websocket.send_json({"status": "position_updated"})
+
+    except WebSocketDisconnect:
+        del active_connections[driver_id]
