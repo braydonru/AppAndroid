7d303bdf8bf058da2a195c3d25f46472bc82c5d1
Braydon <braydonru1234@gmail.com>
2026-04-19 00:31:34 -0500

    cors

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ from config.db import engine
 from sqlmodel import SQLModel
 from models import *
 from routes.driver_router import driver_router
+from fastapi.middleware.cors import CORSMiddleware
 
 SQLModel.metadata.create_all(engine)
 app = FastAPI()
 
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/",tags=["Main"])
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "UberApp Backend"}
 
 app.include_router(driver_router)
